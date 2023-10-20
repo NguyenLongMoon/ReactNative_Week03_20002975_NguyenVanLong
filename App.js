df55db80d7265b616430795c7b16533b44f7b4d7
NguyenLong <longsky0912624119@gmail.com>
2023-10-20 10:14:14 +0700

    Them View, Text roi Style co ban

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View style = {styles.item1}>
+        <Text style = {styles.textcommon}>Hello React Native</Text>
+      </View>
+      <View style = {styles.item2}>
+        <Text style = {styles.textcommon}>Hello React Native</Text>
+      </View>
+      <View style = {styles.item3}>
+        <Text style = {styles.textcommon}>Hello React Native</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +23,38 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  item1:{
+    marginTop: 20,
+    width:100,
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'green'
+
+  },
+  item2:{
+    marginTop: 20,
+    width:100,
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'pink'
+
+  },
+  item3:{
+    marginTop: 20,
+    width:100,
+    height:100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    backgroundColor: 'yellow'
+
+  },
+  textcommon:{
+    fontSize: 20
+  }
 });
